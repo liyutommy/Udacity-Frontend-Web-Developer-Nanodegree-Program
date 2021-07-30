81d7303f90501a5e0535b13c97af2c42b09c1224
Yu Li <liyutommy@outlook.com>
2021-07-30 17:05:24 +0800

    Finish project enhancement

--- evaluate-news-nlp/src/client/js/formHandler.js
+++ evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,16 +1,82 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let formText = document.getElementById('name').value;
+
+    /*
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
+        console.log(res)
         document.getElementById('results').innerHTML = res.message
     })
+    */
+    const dataObj = {
+        text: formText
+    };
+    postData('/textAnalysis', dataObj)
+    .then(() => updateUI());
+    
 }
+
+
+const postData = async (url='', postData={}) => {
+    console.log(postData);
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData),
+    });
+    try{
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch(err){
+        console.log("error", err);
+    }
+}
+
+const results = {
+    'score_tag': {
+        'P+': 'Strong positive',
+        'P': 'Positive',
+        'NEU': 'Neutral',
+        'N': 'Negative',
+        'N+': 'Strong negative',
+        'NONE': 'Without polarity'
+    },
+    'subjectivity': {
+        'OBJECTIVE': 'The text does not have any subjectivity marks.',
+        'SUBJECTIVE': 'The text has subjective marks.'
+    },
+    'irony': {
+        'NONIRONIC': 'The text does not have any irony marks.',
+        'IRONIC': 'The text has irony marks.'
+    }
+}
+
+const updateUI = async () => {
+    const response = await fetch('/results');
+    console.log(response);
+    try{
+        const data = await response.json();
+        document.getElementById('score').innerHTML = `Sentiment Score: ${results.score_tag[data.score_tag]}`;
+        document.getElementById('subject').innerHTML = `Subjectivity: ${results.subjectivity[data.subjectivity]}`;
+        document.getElementById('confidence').innerHTML = `Confidence: ${data.confidence}`;
+        document.getElementById('irony').innerHTML = `Irony: ${results.irony[data.irony]}`;
+    } catch(err){
+        console.log("error", err);
+    }
+
+}
+
+
 
 export { handleSubmit }
--- evaluate-news-nlp/src/server/index.js
+++ evaluate-news-nlp/src/server/index.js
@@ -1,29 +1,63 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const mockAPIResponse = require('./mockAPI.js');
+const fetch = require("node-fetch");
+// require to get environment variable
 const dotenv = require('dotenv');
 dotenv.config();
 
 console.log(`Your API key is ${process.env.API_KEY}`);
 
-const apiKey = '44ffcd966d22e5e35b7a19d983725225';
+const express = require('express');
 
-const app = express()
+const app = express();
+
+const baseURL= 'https://api.meaningcloud.com/sentiment-2.1';
+
+let projectData = {};
+
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
+console.log(__dirname);
 
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
+    res.sendFile('dist/index.html');
     //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+    console.log('Example app listening on port 8081!');
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    console.log(mockAPIResponse);
+    res.send(mockAPIResponse);
+})
+
+app.get('/results', (req, res) => {
+    res.send(projectData);
+})
+
+app.post('/textAnalysis', async (req, res) => {
+    const url = `${baseURL}?key=${process.env.API_KEY}&txt=${req.body.text}&lang=en`;
+    // remove unescaped characters
+    const encodedURI = encodeURI(url);
+    const response = await fetch(encodedURI);
+    try{
+        const data = await response.json();
+        console.log(data);
+        projectData = data;
+        res.send(data);
+    } catch(err){
+        console.log("error", err);
+    }
 })
