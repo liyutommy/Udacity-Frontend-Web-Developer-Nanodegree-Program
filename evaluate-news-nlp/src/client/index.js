74c751bd418477df9568bc6fc4495fa5827a9217
Yu Li <liyutommy@outlook.com>
2021-07-31 00:07:26 +0800

    Add unit tests

--- evaluate-news-nlp/src/client/index.js
+++ evaluate-news-nlp/src/client/index.js
@@ -1,5 +1,5 @@
 import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { handleSubmit, postData } from './js/formHandler'
 
 import './styles/resets.scss'
 import './styles/base.scss'
@@ -16,5 +16,6 @@ console.log("CHANGE!!");
 
 export {
     checkForName,
-    handleSubmit
+    handleSubmit,
+    postData,
 }
--- evaluate-news-nlp/src/client/js/formHandler.js
+++ evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,13 +1,16 @@
+import fetch from 'node-fetch';
+
 function handleSubmit(event) {
     event.preventDefault();
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value;
 
-    /*
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
+
+    /*
     fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
@@ -78,5 +81,5 @@ const updateUI = async () => {
 }
 
 
+export { handleSubmit, postData }
 
-export { handleSubmit }
